2adc688035d3cac1e2ddc07d2918268d99634d99
Dario Maiocchi <dmaiocchi@suse.com>
2016-09-16 12:38:34 +0200

    Fix rubocop lint

--- lib/lavanda.rb
+++ lib/lavanda.rb
@@ -5,5 +5,6 @@ module LavandaBasic
   def run(cmd, user = 'root', timeout = 150)
     out, _lo, _rem, code = test_and_store_results_together(cmd, user, timeout)
     raise "FAIL: command  #{cmd} returned #{code}. output : #{out}" if code != 0
+    [out, code]
   end
 end
