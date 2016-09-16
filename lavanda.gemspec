b083db9c89e617834d6f0b2e6ce1b3fbf80ed316
Dario Maiocchi <dmaiocchi@suse.com>
2016-09-16 12:09:32 +0200

    Rename everything with low L. adding test

---
+++ lavanda.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = "lavanda"
+  s.version     = "0.0.1"
+  s.date        = "2016-09-15"
+  s.summary     = "lavanda gem"
+  s.description = "lavanda ruby testing utilities for multimachines"
+  s.authors     = "Dario Maiocchi"
+  s.email       = "dmaiocchi@suse.com"
+  s.files       = "lib/lavanda.rb"
+  s.license     = "GPL-2.0"
+  s.homepage	= "https://github.com/MalloZup/lavanda"
+end
--- lib/lavanda.rb
+++ lib/lavanda.rb
@@ -1,10 +1,20 @@
 require 'twopence'
 
+
 # this class is do operations  on files that are remote hosts under tests.
-module Rem_File
+module LAVANDA_REMOTE_FILE
       def file_exist(file)
   	_out, _local, _remote, _code = self.test_and_store_results_together("test -f #{file}", "root", 500)
         raise "file doesn't exist" if code != 0
       end
 
 end
+
+
+module LAVANDA_BASIC
+	def run(cmd, user = "root" , timeout = 100)
+  		out, _local, _remote, code = self.test_and_store_results_together(cmd, user, timeout)
+		return out, code
+	end
+end
+
