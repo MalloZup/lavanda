ac50aa50a4f27106bca842ccda33aa3989f9b14f
Dario Maiocchi <dmaiocchi@suse.com>
2016-09-19 11:38:55 +0200

    Dummy fix
    Signed-off-by: Dario Maiocchi <dmaiocchi@suse.com>

--- test/setupTesting.rb
+++ test/setupTesting.rb
@@ -11,10 +11,8 @@ run("sudo touch /root/.ssh/authorized_keys")
 
 # thx to tomas and metan.
 run("sudo cat /root/.ssh/id_rsa.pub | sudo tee /root/.ssh/authorized_keys")
-run("sudo cat /root/.ssh/id_rsa.pub | sudo tee /~/.ssh/authorized_keys")
 # generate ssh key 
 run("sudo chmod -R 600 /root/.ssh")
-run("sudo chmod -R 600 /~/.ssh")
 
 puts("testing  ssh localhost")
 run("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@127.0.0.1 \"whoami\" ")
