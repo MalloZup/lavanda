c88b302b0412873db31a9d3c654b23ff4370eac6
Dario Maiocchi <dmaiocchi@suse.com>
2016-09-19 18:31:15 +0200

    update

--- lib/lavanda.rb
+++ lib/lavanda.rb
@@ -2,7 +2,7 @@ require 'twopence'
 
 # this module monkeypatch some basic commands from twopence
 module LavandaBasic
-  def run(cmd, user = 'root', timeout = 150, fatal = true)
+  def run(cmd, fatal = true, timeout = 200 , user = 'root')
     out, _lo, _rem, code = test_and_store_results_together(cmd, user, timeout)
     if fatal
       raise "FAIL: #{cmd} returned #{code}. output : #{out}" if code != 0
