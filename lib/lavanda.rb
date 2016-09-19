a5603925c94760cff944d0f7c8aa9b109ff2f9e6
Dario Maiocchi <dmaiocchi@suse.com>
2016-09-19 18:16:03 +0200

    Addin fatal flag

--- lib/lavanda.rb
+++ lib/lavanda.rb
@@ -2,9 +2,11 @@ require 'twopence'
 
 # this module monkeypatch some basic commands from twopence
 module LavandaBasic
-  def run(cmd, user = 'root', timeout = 150)
+  def run(cmd, user = 'root', timeout = 150, fatal = true)
     out, _lo, _rem, code = test_and_store_results_together(cmd, user, timeout)
-    raise "FAIL: command  #{cmd} returned #{code}. output : #{out}" if code != 0
+    if fatal
+      raise "FAIL: #{cmd} returned #{code}. output : #{out}" if code != 0
+    end
     [out, code]
   end
 end
