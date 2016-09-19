631cc74a65b000b3d448878c7d25690daf8030ba
Dario Maiocchi <dmaiocchi@suse.com>
2016-09-19 11:31:06 +0200

    fix
    Signed-off-by: Dario Maiocchi <dmaiocchi@suse.com>

--- test/setupTesting.rb
+++ test/setupTesting.rb
@@ -7,12 +7,13 @@ def run(cmd)
 end
 
 run("sudo ssh-keygen -t rsa -N \"\" -f /root/.ssh/id_rsa")
-run("sudo touch /root/.ssh/authorized_keys")
-puts "Autorized keys fiel created"
 # thx to tomas and metan.
 run("sudo cat /root/.ssh/id_rsa.pub | sudo tee /root/.ssh/authorized_keys")
+run("sudo cat /root/.ssh/id_rsa.pub | sudo tee /~/.ssh/authorized_keys")
 # generate ssh key 
 run("sudo chmod -R 600 /root/.ssh")
+run("sudo chmod -R 600 /~/.ssh")
+
 puts("testing  ssh localhost")
 run("ssh root@127.0.0.1 \"whoami\" ")
 puts("*********** SETTING UP FOR TWOPENCE DONE *********")
